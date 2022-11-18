e0cb9065c348e9a37bc670075d26e22d124849d5
Lionel Yu <lionel@musicalbasics.com>
2022-11-17 18:07:15 -0800

    Initial Commit

---
+++ Elephant-V1UITests/Elephant_V1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Elephant_V1UITestsLaunchTests.swift
+//  Elephant-V1UITests
+//
+//  Created by Lionel Yu on 11/17/22.
+//
+
+import XCTest
+
+class Elephant_V1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
